7261efffc7605556ae94c456833dd1e1cad11f44
Jorge Eduardo Ponce Leyva <jorge.ponce@ift.org.mx>
2021-12-16 04:11:02 -0600

    Mapping Lines

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -17,11 +17,14 @@ let line = [
 
 // Create a polyline using the line coordinates and make the line red.
 L.polyline(line, {
-  color: "yellow"
+  color: "blue", 
+  weight: 1,
+  opacity: 0.8,
+  dashArray: '20, 20'
 }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
